476977318996ed694806a39bd14a74d1b67e1f75
Lars Wikman <lars.wikman@gmail.com>
2020-05-05 15:20:08 +0200

    Experimental k3s system changes based on mobileoverlords docker changes

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
 defmodule NervesSystemRpi4.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi4
+  @github_organization "lawik"
+  @app :nerves_system_rpi4_k3s
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
            |> String.trim()
